4950c4ba7aff129db51c5482b95f7307adb8653a
kalyani.deshmukh <kalyani.deshmukh@sjsu.edu>
2020-07-10 12:53:28 -0700

    updated mongodb connection

--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator= require('validator')
 const bcrypt = require('bcryptjs')
-const jwt= require('jsonwebtoken')
+const jwt= require('jsonwebtoken') 
 const Task= require('./tasks')
 const userSchema= new mongoose.Schema({
     name:{
